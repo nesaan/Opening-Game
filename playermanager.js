4da2a0053fa3db2b4e4ccb26b5955f736baacd98
nesaan <nesaan@gmail.com>
2019-03-20 18:36:34 -0400

    Score Manager to improve synch between players
    * Score Manager to improve synch between players
    
    * Changed player database array to dictionary
    
    * Improved players iter and removed uuid property.
    
    * Typo in players
    
    * Un-Kev-Bad
    
    * forgot to remove .keys
    
    * for of even

---
+++ playermanager.js
@@ -0,0 +1,73 @@
+var PlayerManager = function (){
+  var players;
+  function init(){
+    players = {};
+  }
+
+  function join(socket, uuid, name){
+
+    if (players[uuid]){
+      return;
+    }
+
+    for (var playKey of Object.keys(players)){
+      var player = players[playKey];
+      socket.emit('addscore', {
+        uuid:playKey,
+        score:player.score,
+        username:player.username
+      });
+
+      player.socket.emit('addscore',  {
+        uuid:uuid,
+        username:name,
+        score:0
+      });
+    }
+
+    socket.emit('addscore', {
+      uuid:uuid,
+      score:0,
+      username:name
+    });
+
+    players[uuid] = {
+      username:name,
+      score:0,
+      socket:socket
+    };
+  }
+
+  function remove(uuid){
+    if (players[uuid]){
+      for (var playKey of Object.keys(players)){
+        players[playKey].socket.emit('removescore', {
+          uuid:uuid
+        });
+      }
+      players[uuid].socket.emit('removeallscores');
+      delete players[uuid];
+    }
+  }
+
+  function update(uuid, score){
+    if (players[uuid]){
+      for (var playKey of Object.keys(players)){
+        players[playKey].socket.emit('updatescore', {
+          uuid:uuid,
+          score:score
+        });
+      }
+      players[uuid].score = score;
+    }
+  }
+
+  return {
+    update:update,
+    remove:remove,
+    join:join,
+    init:init
+  };
+}();
+
+module.exports = PlayerManager;
